848938efaeb38fdcc6e87e5fdcdc12e941494e5e
DJDQfff <10173363+DJDQfff@user.noreply.gitee.com>
2022-03-11 07:55:48 +0800

    MangaBook实例化是异常捕获（粗略）

--- 本子管理器/Models/CollectionObserver.cs
+++ 本子管理器/Models/CollectionObserver.cs
@@ -50,13 +50,11 @@ namespace EroMangaManager.Models
         }
 
         /// <summary>
-        /// 添加文件夹，并添加文件夹及下属漫画漫画到集合
-        /// 1.添加改文件夹到系统API
-        /// 2.添加文件夹到FolderList
-        /// 3.添加文件夹下属漫画到MangaList
+        /// 添加文件夹，并添加文件夹及下属漫画漫画到集合 1.添加改文件夹到系统API
+        /// 2.添加文件夹到FolderList 3.添加文件夹下属漫画到MangaList
         /// </summary>
-        /// <param name="folder"></param>
-        /// <returns></returns>
+        /// <param name="folder"> </param>
+        /// <returns> </returns>
         public async Task AddFolder (StorageFolder folder)
         {
             FutureAccessList.Add(folder);
@@ -71,12 +69,10 @@ namespace EroMangaManager.Models
         }
 
         /// <summary>
-        /// 移除文件夹，并从集合中移除文件夹及下属漫画
-        /// 1.从系统API中移除
-        /// 2.从FolderList里移除
-        /// 3.从MangaList里移除文件夹下属漫画
+        /// 移除文件夹，并从集合中移除文件夹及下属漫画 1.从系统API中移除
+        /// 2.从FolderList里移除 3.从MangaList里移除文件夹下属漫画
         /// </summary>
-        /// <param name="folder"></param>
+        /// <param name="folder"> </param>
         public void RemoveFolder (StorageFolder folder)
         {
             FolderList.Remove(folder);      // 从访问列表Model里移除
@@ -93,11 +89,9 @@ namespace EroMangaManager.Models
             }
         }
 
-        /// <summary>
-        /// 提取文件夹的下属漫画到MangaList
-        /// </summary>
-        /// <param name="storageFolder"></param>
-        /// <returns></returns>
+        /// <summary> 提取文件夹的下属漫画到MangaList </summary>
+        /// <param name="storageFolder"> </param>
+        /// <returns> </returns>
         private async Task PickMangasInFolder (StorageFolder storageFolder)
         {
             var files = await storageFolder.GetFilesAsync();
@@ -105,8 +99,7 @@ namespace EroMangaManager.Models
 
             List<ReadingInfo> add = new List<ReadingInfo>();
             // TODO：这里如果使用 list<task> 的话，会出bug
-            // 普通的遍历添加反而不会出现bug
-            // bug 名称：已为另一线程调用
+            // 普通的遍历添加反而不会出现bug bug 名称：已为另一线程调用
             for (int i = 0; i < files.Count; i++)
             {
                 ReadingInfo readingInfo;
@@ -119,13 +112,21 @@ namespace EroMangaManager.Models
                     readingInfo = ReadingInfoFactory.Creat(files[i].Path);
                     add.Add(readingInfo);
                 }
-                MangaBook manga = new MangaBook(files[i], storageFolder, readingInfo);
+                try
+                {
+                    MangaBook manga = new MangaBook(files[i], storageFolder, readingInfo);
 
-                MangaList.Add(manga);         // 多线程下，对list进行写操作，会出bug
+                    MangaList.Add(manga);
 
-                await manga.EnsureCoverFile();
+                    await manga.EnsureCoverFile();
 
-                await manga.SetCover();
+                    await manga.SetCover();
+                }
+                catch (Exception)
+                {
+                    //不是正常本子文件
+                    // TODO 先不管，以后再专门做一个类来管理失败的文件
+                }
             }
 
             await DatabaseController.ReadingInfo_AddMulti(add);
@@ -133,9 +134,7 @@ namespace EroMangaManager.Models
 
         #region 弃用
 
-        /// <summary>
-        /// 读取数据库，将指定文件夹下的 MangaTag 移除
-        /// </summary>
+        /// <summary> 读取数据库，将指定文件夹下的 MangaTag 移除 </summary>
         /// <param name="storageFolder"> </param>
         /// <returns> </returns>
         private async Task RemoveMultiTagsFromDatabase (StorageFolder storageFolder)
@@ -144,11 +143,9 @@ namespace EroMangaManager.Models
             await DatabaseController.MangaTag_RemoveMulti(files);
         }
 
-        /// <summary>
-        /// 从数据库中移除指定漫画的 MangaTag
-        /// </summary>
-        /// <param name="mangaBook"></param>
-        /// <returns></returns>
+        /// <summary> 从数据库中移除指定漫画的 MangaTag </summary>
+        /// <param name="mangaBook"> </param>
+        /// <returns> </returns>
         public async Task DeleteSingleMangaBook (MangaBook mangaBook)
         {
             await mangaBook.StorageFile.DeleteAsync();
