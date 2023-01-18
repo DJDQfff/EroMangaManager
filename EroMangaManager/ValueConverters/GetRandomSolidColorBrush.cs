725f6996f6d79d049947929cc34316b7a5d30fe6
DJDQfff <2743109998@qq.com>
2023-01-18 10:00:47 +0800

    添加一个随机颜色制转换器

---
+++ EroMangaManager/ValueConverters/GetRandomSolidColorBrush.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EroMangaManager.ValueConverters
+{
+    internal class GetRandomSolidColorBrush:Windows.UI.Xaml.Data.IValueConverter
+    {
+        public object Convert (object value , Type targetType , object parameter , string language)
+        {
+            var brush = MyUWPLibrary.WindowsUIColorHelper.GetRandomSolidColorBrush();
+            return brush;
+        }
+
+        public object ConvertBack (object value , Type targetType , object parameter , string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
